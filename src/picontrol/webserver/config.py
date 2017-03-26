1821e5e8bd9a81c05e326ac0587465c1fe20abaa
Scott Lemmon <tlemmon@tesla.com>
2017-03-25 19:37:11 -0700

    Move configs from source code
    Configs should live in a separte location from source code.  In this
    case we will put it in the root project directory, but will expect them
    to live in the ".picontrol" direcotry in the users home.  This is very
    standard for many systems like this.
    
    It will also read from the old configs location, but will not write to
    it anymore, and will not read the old location if the new location
    exists.

--- src/picontrol/webserver/config.py
+++ src/picontrol/webserver/config.py
@@ -1,38 +1,30 @@
 import sys, ConfigParser
+import picontrol
+import os
 
-basePath = '/home/pi/scripts/picontrol/configs'
-updatePath = '/home/pi/scripts/picontrol_update/picontrol/configs'
+CONFIG_PATHS = ["~/.picontrol", "~/scripts/picontrol/configs"]
+UPDATE_PATHS = ["~/.picontrol_update", "~/scripts/picontrol_update/picontrol/configs"]
 
 class Config():
     @staticmethod
     def loadConfig():
         config = ConfigParser.RawConfigParser()
-        configFilePath = basePath + '/config.conf'
-        config.read(configFilePath)
+        config.read([os.join(path, 'config.conf') for path in CONFIG_PATHS])
         return config
-    
+
     @staticmethod
     def saveConfig(config):
-        with open(basePath + '/config.conf', 'w') as configFile:
+        with open(os.join(CONFIG_PATHS[0], 'config.conf'), 'w') as configFile:
             config.write(configFile)
-        return True
 
     @staticmethod
     def loadVersion():
         config = ConfigParser.RawConfigParser()
-        configFilePath = basePath + '/picontrol.version'
-        config.read(configFilePath)
+        config.read([os.join(path, 'picontrol.version') for path in CONFIG_PATHS])
         return config
-    
-    @staticmethod
-    def saveVersion(config):
-        with open(basePath + '/picontrol.version', 'w') as configFile:
-            config.write(configFile)
-        return True
 
     @staticmethod
     def loadUpdateVersion():
         config = ConfigParser.RawConfigParser()
-        configFilePath = updatePath + '/picontrol.version'
-        config.read(configFilePath)
+        config.read([os.join(path, 'picontrol.version') for path in UPDATE_PATHS])
         return config
